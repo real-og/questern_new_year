72e32fd0dc52565e7d744c46e5244d2e3d78a59e
Evgeniy Grakovich <grakovich-e@yandex.by>
2024-01-04 22:52:34 +0300

    debug excluded

--- handlers/commands.py
+++ handlers/commands.py
@@ -8,9 +8,7 @@ import keyboards as kb
 @dp.message_handler(commands=['start'], state="*")
 async def send_welcome(message: types.Message, state: FSMContext):
     await message.answer(texts.start, reply_markup=kb.start_quest_kb)
-    # await State.starting.set()
-    await state.update_data(gifts=[])
-    await State.collecting_gifts.set()
+    await State.starting.set()
     
 
 @dp.message_handler(commands=['help'], state="*")
